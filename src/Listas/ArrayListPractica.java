9b5a136999b2c83e4f2071ad30af73ef7c78300b
erisjimver <erisjinver@gmail.com>
2021-03-03 09:24:00 -0500

    Lectura del array List Exitosa

--- src/Listas/ArrayListPractica.java
+++ src/Listas/ArrayListPractica.java
@@ -1,6 +1,7 @@
 package Listas;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class ArrayListPractica {
     
@@ -8,7 +9,7 @@ public class ArrayListPractica {
    
     public void registar(){
         try{
-       //     String [] cortarString = linea.split("::");		// Obtengo los datos del partido de futbol
+            
             Datos productos = new Datos();	// Creo un objeto de la clase "Datos productos"
             
             //agregar atributos del objeto productos//
@@ -26,4 +27,38 @@ public class ArrayListPractica {
         }
     }
     
+    public void registarVista(String codigo, String nombre, String precio_compra, String precio_venta, String stock, String cantidad_vendida){
+        try{
+
+            Datos productos = new Datos();	// Creo un objeto de la clase "Datos productos"
+            
+            //agregar atributos del objeto productos//
+            productos.setCodigo_producto(codigo);
+            productos.setNombre_producto(nombre);
+            productos.setPrecio_compra(precio_compra);
+            productos.setPrecio_venta(precio_venta);
+            productos.setStock(stock);
+            productos.setCantidad_vendida(cantidad_vendida);
+            
+            lista.add(productos);
+            
+        }catch(Exception e){
+            System.out.println("Error: "+e);
+        }
+    }   
+    public void leer(){
+        System.out.println("n... Resultados de los partidos de Futbol ...");
+        Iterator<Datos> itrPartidos = lista.iterator();
+        while(itrPartidos.hasNext()){
+	Datos partido = itrPartidos.next();
+	System.out.println(partido.getCodigo_producto() + " "
+			+ partido.getNombre_producto() + " "
+			+ partido.getPrecio_compra() + " "
+			+ partido.getPrecio_venta() + " "
+                        + partido.getStock() + " "
+                        + partido.getCantidad_vendida());
+    
+        }
+    
+}
 }
--- src/Principal/ejecutora.java
+++ src/Principal/ejecutora.java
@@ -25,7 +25,7 @@ public class ejecutora {
             */
     
     listas.registar();
-        
+    listas.leer();
         
     }
     
